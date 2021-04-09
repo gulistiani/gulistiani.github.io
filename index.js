918f9e7dc4b8d47fe940a98f58bbd19473b72b56
gulistiani <gulistiani.fuzuli@esdm.go.id>
2021-04-09 15:42:25 +0700

    lumayan

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 var playpause = false
-document.getElementById("jawaban").style.display = "none"
-document.getElementById("card").style.display = "none"
-document.getElementById("jawabanBtn").style.visibility = "hidden"
-document.getElementById("questionBtn").style.visibility = "hidden";
-document.getElementById("resetBtn").style.visibility = "hidden";
+document.getElementById("container").style.display = "none"
+document.getElementById("container_star").style.display = "none"
+document.getElementById("resetBtn").style.visibility = "hidden"
+document.getElementById("berhasilBtn").style.visibility = "hidden"
+document.getElementById("gagalBtn").style.visibility = "hidden"
 var detailSurat = []
 var pilihanStart = 1
 var pilihanEnd = 1
 var randomNumber = 1
 var randomAyat = 1
 var randomOK = []
-var berhasil = 0
+var berhasilBtn = 0
 var status = []
 var counter = 0
 var jmlTest = 0
+let arrStar = []
+let insertStarYes = `<img class="star" src="img/yes.png">`
+let insertStarNo = `<img class="star" src="img/no.png">`
+let insertStarNetral = `<img class="star" src="img/netral.png">`
+let jmlGagal = 0
+let jmlBerhasil = 0
+let jmlNetral = 0
 
 function togglePlay() {
+    arrStar = []
     counter = 0
-    document.getElementById("jawaban").style.display = "none"
+    jmlGagal = 0
+    jmlBerhasil = 0
     document.getElementById("surat").disabled = true
     document.getElementById("start").disabled = true
     document.getElementById("end").disabled = true
@@ -25,9 +34,8 @@ function togglePlay() {
     document.getElementById("denganAudio").disabled = true
     document.getElementById("tanpaAudio").disabled = true
     document.getElementById("myButton").disabled = true
-    document.getElementById("jawabanBtn").style.visibility = "hidden"
     document.getElementById('ayatJawaban').innerHTML = ''
-    document.getElementById('demo').innerHTML = ''
+    document.getElementById('ayatSoal').innerHTML = ''
 
     pilihanStart = Number(document.getElementById("start").value)
     pilihanEnd = Number(document.getElementById("end").value)
@@ -37,105 +45,165 @@ function togglePlay() {
     detailSurat = getDetailSurat(pilihanSurat)
 
     if (pilihanStart < 1) {
-        alert(`Nomor ayat ${start} tidak valid`)
+        showError(`Nomor ayat ${pilihanStart} tidak valid`)
     } else if (pilihanEnd > detailSurat[0]) {
-        alert(`Nomor ayat ${end} tidak valid untuk surat ${pilihanSurat}`)
+        showError(`Nomor ayat ${pilihanEnd} tidak valid untuk surat ${pilihanSurat}`)
+    } else if (jmlTest > pilihanEnd - pilihanStart + 1) {
+        showError(`Jumlah test (${jmlTest}) melebihi range ayat (dari ayat ${pilihanStart} ke ayat ${pilihanEnd} cuma ada ${pilihanEnd - pilihanStart + 1} ayat)`)
     } else {
-        document.getElementById("questionBtn").style.visibility = "visible"
         document.getElementById("resetBtn").style.visibility = "visible"
-
         randomNumber = getRndInteger(pilihanStart, pilihanEnd)
         let randomNumberUnik = randomUniqueNum(pilihanStart, pilihanEnd)
         randomOK = randomNumberUnik.slice(0, jmlTest)
-
+        startAnimationIn(document.getElementById("container"))
+        playQuestion()
     }
+}
+
+function nextQuestion() {
+    startAnimationOut(document.getElementById("container"))
+
+    if (counter < jmlTest) {
+        setTimeout(function () {
+            startAnimationIn(document.getElementById("container"))
+        }, 1500)
+    }
+    playQuestion()
 }
 
 function playQuestion() {
-    startanimation(document.getElementById("card"))
-    document.getElementById("card").style.display = "block"
-    document.getElementById('ayatJawaban').innerHTML = ''
-    document.getElementById("jawaban").style.display = "none"
-    document.getElementById("jawabanBtn").style.visibility = "visible"
+    document.getElementById("berhasilBtn").style.visibility = "visible";
+    document.getElementById("gagalBtn").style.visibility = "visible";
+    document.getElementById("container").style.display = "block"
+    document.getElementById("container_star").style.display = "block"
     randomAyat = Number(detailSurat[5]) + Number(randomOK[counter])
 
-    let fileAudio = `quran/${detailSurat[1].toString().padStart(3, '0')}${randomOK[counter].toString().padStart(3, '0')}.mp3`
-    document.getElementById('demo').innerHTML = `Soal ke ${counter + 1} dari ${jmlTest}`
-    document.getElementById("card").innerText = data[randomAyat - 1].pendek
+    let totalStar = ''
+    for (let i = 0; i < arrStar.length; i++) {
+        totalStar = totalStar + arrStar[i]
+    }
+    for (let i = 0; i < jmlTest - arrStar.length; i++) {
+        totalStar = totalStar + insertStarNetral
+    }
+    document.getElementById("container_star").innerHTML = totalStar
 
-    if (document.getElementById("denganAudio").checked === true) {
-        let audio = document.getElementById("myAudio")
-        audio.src = fileAudio
+    if (counter < jmlTest) {
+        let fileAudio = `data/mp3/${detailSurat[1].toString().padStart(3, '0')}${randomOK[counter].toString().padStart(3, '0')}.mp3`
+        document.getElementById('ayatSoal').innerText = `Soal ke-${counter + 1} dari ${jmlTest} soal`
+        document.getElementById("soal").innerText = data[randomAyat - 1].pendek
 
-        if (playpause === false) {
-            document.getElementById("questionBtn").disabled = true
+        document.getElementById('ayatJawaban').innerText = `Kunci jawaban: Surat ${detailSurat[2]} ayat ${randomOK[counter]}`
+        document.getElementById("jawaban").innerText = data[randomAyat - 1].panjang
 
-            setTimeout(function () {
-                audio.play()
-                document.getElementById("questionBtn").disabled = true
+        if (document.getElementById("denganAudio").checked === true) {
+            let audio = document.getElementById("myAudio")
+            audio.src = fileAudio
+
+            if (playpause === false) {
+                document.getElementById("berhasilBtn").disabled = true
+                document.getElementById("gagalBtn").disabled = true
 
                 setTimeout(function () {
-                    audio.pause()
-                    audio.currentTime = 0
-                    document.getElementById("questionBtn").disabled = false
-                }, 4000)
-            }, 1000)
+                    setTimeout(function () {
+                        audio.play()
+                    }, 1000)
 
+                    document.getElementById("berhasilBtn").disabled = true
+                    document.getElementById("gagalBtn").disabled = true
+
+                    setTimeout(function () {
+                        audio.pause()
+                        audio.currentTime = 0
+                        document.getElementById("berhasilBtn").disabled = false
+                        document.getElementById("gagalBtn").disabled = false
+                    }, 4500)
+                }, 1000)
+
+            }
+            else {
+                document.getElementById("berhasilBtn").disabled = false
+                document.getElementById("gagalBtn").disabled = false
+                audio.pause()
+            }
+
+        } else {
+            if (playpause === false) {
+                document.getElementById("berhasilBtn").disabled = true
+                document.getElementById("gagalBtn").disabled = true
+                setTimeout(function () {
+                    document.getElementById("berhasilBtn").disabled = true
+                    document.getElementById("gagalBtn").disabled = true
+                    setTimeout(function () {
+                        document.getElementById("berhasilBtn").disabled = false
+                        document.getElementById("gagalBtn").disabled = false
+                    }, 100)
+                }, 0)
+
+            }
+            else {
+                document.getElementById("berhasilBtn").disabled = false
+                document.getElementById("gagalBtn").disabled = false
+            }
         }
-        else {
-            document.getElementById("questionBtn").disabled = false
-            audio.pause()
+
+        counter++
+        if (counter >= jmlTest) {
+            document.getElementById("resetBtn").style.visibility = "hidden"
+            document.getElementById("myButton").style.visibility = "visible"
+            document.getElementById("surat").disabled = false
+            document.getElementById("start").disabled = false
+            document.getElementById("end").disabled = false
+            document.getElementById("jmlTest").disabled = false
+            document.getElementById("denganAudio").disabled = false
+            document.getElementById("tanpaAudio").disabled = false
+            //document.getElementById("myButton").disabled = false
+
         }
 
     } else {
-        if (playpause === false) {
-            document.getElementById("questionBtn").disabled = true
-            setTimeout(function () {
-                document.getElementById("questionBtn").disabled = true
-                setTimeout(function () {
-                    document.getElementById("questionBtn").disabled = false
-                }, 100)
-            }, 0)
-
-        }
-        else {
-            document.getElementById("questionBtn").disabled = false
-        }
-    }
-
-    counter++
-    console.log(counter)
-    if (counter >= Number(document.getElementById("jmlTest").value)) {
-        document.getElementById("questionBtn").style.visibility = "hidden"
-        document.getElementById("resetBtn").style.visibility = "hidden"
-        document.getElementById("myButton").style.visibility = "visible"
-        document.getElementById("surat").disabled = false
-        document.getElementById("start").disabled = false
-        document.getElementById("end").disabled = false
-        document.getElementById("jmlTest").disabled = false
-        document.getElementById("denganAudio").disabled = false
-        document.getElementById("tanpaAudio").disabled = false
+        document.getElementById("berhasilBtn").style.visibility = "hidden"
+        document.getElementById("gagalBtn").style.visibility = "hidden"
         document.getElementById("myButton").disabled = false
-
+        setTimeout(function () {
+            showModal()
+        }, 1000)
     }
 }
 
-function startanimation(element) {
-    element.classList.add("bounce");
+function startAnimationIn(element) {
+    document.getElementById("container").style.visibility = "visible"
+    element.classList.add("rollIn");
+    element.classList.add("roll-in");
     element.addEventListener("animationend", function () {
-        element.classList.remove("bounce");
+        element.classList.remove("rollIn");
+        document.getElementById("container").style.visibility = "visible"
     });
-
 }
+
+function startAnimationOut(element) {
+    element.classList.add("rollOut");
+    element.classList.add("roll-out");
+    element.addEventListener("animationend", function () {
+        element.classList.remove("rollOut");
+        document.getElementById("container").style.visibility = "hidden"
+    });
+}
+
+function gagal() {
+    arrStar.push(insertStarNo)
+    jmlGagal++
+    nextQuestion()
+}
+
+function berhasil() {
+    arrStar.push(insertStarYes)
+    jmlBerhasil++
+    nextQuestion()
+}
+
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min
-}
-
-function showJawaban() {
-    document.getElementById("jawaban").style.display = "block"
-    document.getElementById("jawaban").innerText = data[randomAyat - 1].panjang
-    document.getElementById('ayatJawaban').innerHTML = `Surat ${detailSurat[2]} ayat ${randomOK[counter - 1]}`
 }
 
 function randomUniqueNum(start, end) {
@@ -166,10 +234,12 @@ function shuffle(array) {
 
 
 function resetQuestion() {
-    document.getElementById("questionBtn").style.visibility = "hidden"
+    document.getElementById("gagalBtn").style.visibility = "hidden"
+    document.getElementById("berhasilBtn").style.visibility = "hidden"
     document.getElementById("resetBtn").style.visibility = "hidden"
-    document.getElementById("jawabanBtn").style.visibility = "hidden"
     document.getElementById("myButton").style.visibility = "visible"
+    document.getElementById("berhasilBtn").style.visibility = "hidden"
+    document.getElementById("berhasilBtn").style.visibility = "hidden"
     document.getElementById("surat").disabled = false
     document.getElementById("start").disabled = false
     document.getElementById("end").disabled = false
@@ -177,13 +247,13 @@ function resetQuestion() {
     document.getElementById("denganAudio").disabled = false
     document.getElementById("tanpaAudio").disabled = false
     document.getElementById("myButton").disabled = false
-    document.getElementById('demo').innerHTML = ''
-    document.getElementById("jawaban").style.display = "none"
-    document.getElementById("ayatJawaban").innerText = ''
-    document.getElementById("card").style.display = "none"
-    document.getElementById("jawaban").style.display = "none"
-
-
+    document.getElementById('ayatSoal').innerHTML = ''
+    document.getElementById('ayatSoal').innerText = ''
+    document.getElementById("soal").innerText = ''
+    document.getElementById('ayatJawaban').innerText = ''
+    document.getElementById("jawaban").innerText = ''
+    document.getElementById("container").style.display = "none"
+    document.getElementById("container_star").style.display = "none"
 }
 
 function getSurat() {
@@ -207,4 +277,53 @@ function getDetailSurat(surat) {
         }
     }
     return [Number(jmlAyat), no_surat, nama_surat, arti_surat, jenis, startAyat, endAyat]
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function showError(errMsg) {
+    document.getElementById('pesan').innerHTML = 'Invalid Data'
+    document.getElementById('nilai').innerText = errMsg
+    document.getElementById('footer').innerText = 'Coba dicek lagi ...'
+    modal.style.display = "block";
+
+    document.getElementById("surat").disabled = false
+    document.getElementById("start").disabled = false
+    document.getElementById("end").disabled = false
+    document.getElementById("jmlTest").disabled = false
+    document.getElementById("denganAudio").disabled = false
+    document.getElementById("tanpaAudio").disabled = false
+    document.getElementById("myButton").disabled = false
+}
+
+function showModal() {
+    let score = Math.round(jmlBerhasil / jmlTest * 100)
+    if (score >= 80) {
+        document.getElementById('pesan').innerHTML = '&#127881; Subhanallah, kamu hebat! &#128077;'
+    } else if (score >= 60 && score < 80) {
+        document.getElementById('pesan').innerHTML = 'Ayo sedikit lagi... &#127803;'
+    } else {
+        document.getElementById('pesan').innerHTML = 'Jangan sedih, kamu pasti bisa! &#128521;'
+    }
+
+    document.getElementById('nilai').innerText = `Nilai kamu ${score}`
+    document.getElementById('footer').innerText = `Semangat terus ya anak sholeh/sholehah ...`
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
